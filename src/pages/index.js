7c7883e3338a30e22cda586e58469811925835cc
Mark Cramer <mecramer@users.noreply.github.com>
2020-04-18 20:27:41 -0400

    Clean up home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => {
             <h1>Hello</h1>
             <h2>I'm Mark, learning Gatsby</h2>
             <p>
-                <Link to="/contact">Contact me</Link> for more information.
+                Need some help? <Link to="/contact">Contact me</Link> for more information.
             </p>
         </Layout>
     );
